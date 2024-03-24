497a9331fba3b581cbed40c5e61c584cf2b4b69b
Почемучка <159887975+Maria-Kipurova@users.noreply.github.com>
2024-03-24 18:14:42 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,38 +1,52 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int* arr, int size, int value) {
-    int count = 0;
-    int left = 0;
-    int right = size - 1;
-    
-    while (left <= right) {
-        int mid = (left + right) / 2;
-        
-        if (arr[mid] == value) {
-            count++;
-            
-            // Подсчет всех найденных элементов со значением value
-            int i = mid - 1;
-            while (i >= left && arr[i] == value) {
-                count++;
-                i--;
-            }
-            
-            i = mid + 1;
-            while (i <= right && arr[i] == value) {
-                count++;
-                i++;
-            }
-            
-            break;
-        }
-        else if (arr[mid] < value) {
-            left = mid + 1;
-        }
-        else {
-            right = mid - 1;
-        }
+  int left = 0, right = size - 1;
+  int centr = (right - left + 1) / 2, counter = 0;
+  int stopp = 0;
+  while (arr[centr] != value) {
+    if (arr[centr] < value) {
+      left = centr;
+    } else if (arr[centr] > value) {
+      right = centr;
     }
-    
-    return count;
+    centr = (right - left) / 2;
+    if (right - left <= 1) {
+      counter -= 1;
+      break;
+    }
+    stopp += 1;
+    if (stopp == 10) {
+      return 0;
+    }
+  }
+  counter += 1;
+  bool one = true, two = true;
+  int index1 = centr - 1, index2 = centr + 1;
+  while (true) {
+    if (one) {
+      if (index1 < 0 || index1 > size - 1) {
+        one = false;
+      } else {
+        if (arr[index1] == value) {
+          counter += 1;
+        }
+      }
+    }
+    if (two) {
+      if (index2 < 0 || index2 > size - 1) {
+        two = false;
+      } else {
+        if (arr[index2] == value) {
+          counter += 1;
+        }
+      }
+    }
+    index1 -= 1;
+    index2 += 1;
+    if (!one && !two) {
+      break;
+    }
+  }
+  return counter;  // если ничего не найдено
 }
