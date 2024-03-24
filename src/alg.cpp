cedb0b419ddeeb173f02550ad1e492115da24e66
Почемучка <159887975+Maria-Kipurova@users.noreply.github.com>
2024-03-24 18:03:10 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,6 @@
 // Copyright 2021 NNTU-CS
 
-int cbinsearch(int *arr, int size, int value) {
+int cbinsearch(int* arr, int size, int value) {
     int count = 0;
     int left = 0;
     int right = size - 1;
@@ -8,10 +8,22 @@ int cbinsearch(int *arr, int size, int value) {
         int mid = (left + right) / 2;
         if (arr[mid] == value) {
             count++;
+            int i = mid - 1;
+            while (i >= left && arr[i] == value) {
+                count++;
+                i--;
+            }
+            i = mid + 1;
+            while (i <= right && arr[i] == value) {
+                count++;
+                i++;
+            }
+            break;
         }
-        if (arr[mid] <= value) {
+        else if (arr[mid] < value) {
             left = mid + 1;
-        } else {
+        }
+        else {
             right = mid - 1;
         }
     }
