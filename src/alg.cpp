85351860218660d76c76c77981f68b9c713a5b27
Почемучка <159887975+Maria-Kipurova@users.noreply.github.com>
2024-03-24 12:27:33 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,16 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+    int count = 0;
+    int left = 0;
+    int right = size - 1;
+    while (left <= right) {
+        int mid = (left + right) / 2;
+        if (arr[mid] == value) {
+            count++;
+        }
+        left = (arr[mid] < value) ? mid + 1 : left;
+        right = (arr[mid] > value) ? mid - 1 : right;
+    }
+    return count;
 }
